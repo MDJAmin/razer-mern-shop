57c7cff827c23a31da7c94a189777c8ce826ceff
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-09-18 00:57:53 +0430

    feat: add user routers

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,9 @@ import path from "path";
 import HandleError from "./Utils/handleError.js";
 import catchError from "./Utils/catchError.js";
 
+import userRouter from "./Routes/user.route.js"
+
+
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 
@@ -16,6 +19,9 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.static("Public"));
+
+// Routes
+app.use('/api/user', userRouter)
 
 app.use("*", (req, res, next) => {
   return next(new HandleError("Route not found", 404));
