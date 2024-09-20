ee1907afca8fc9b48cf7679f1e9c78b4682b9603
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-09-20 04:43:14 +0430

    feat: add slider model and controller

---
+++ backend/Models/slider.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const sliderSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [
+      true,
+      {
+        en: "title is required",
+        fa: "عنوان الزامی می باشد",
+      },
+    ],
+  },
+  image: {
+    type: String,
+    required: [
+      true,
+      {
+        en: "image is required",
+        fa: "تصویر الزامی می باشد",
+      },
+    ],
+  },
+  href: {
+    type: String,
+  },
+  position: {
+    type: String,
+    default: "home",
+  },
+});
+
+const Slider = mongoose.model("Slider", sliderSchema);
+export default Slider;
