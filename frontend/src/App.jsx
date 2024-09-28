2b06e23b91eada3a8272682753fe1232ba29dc9a
MDJAmin <mohammadamindf@gmail.com>
2024-09-28 19:48:34 +0330

    Test : Gsap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import {Home, Auth, Category, ProductDetails} from "../src/Constants"
+import React, { useEffect } from 'react';
+import { Home, Auth, Category, ProductDetails } from "../src/Constants";
+import gsap from "gsap";
 
 export default function App() {
+  
+  useEffect(() => {
+    gsap.fromTo(
+      ".app-title", 
+      { opacity: 0, y: -50 }, 
+      { opacity: 1, y: 0, duration: 1 }  
+    );
+  }, []);
+  
   return (
     <>
-    <div>App</div>
-    <Home/>
-    <Auth/>
-    <Category/>
-    <ProductDetails/>
+      <div className="app-title">App</div>
+      <Home />
+      <Auth />
+      <Category />
+      <ProductDetails />
     </>
-  )
+  );
 }
