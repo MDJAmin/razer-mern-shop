20b8ec77ea4646912716f8eca6ee57a5c99509a2
MDJAmin <mohammadamindf@gmail.com>
2024-09-28 19:47:55 +0330

    Feat : Add pages
    basic pages and component folder with Constants for better code structure

---
+++ frontend/src/Constants/index.js
@@ -0,0 +1,6 @@
+import Home  from "../Pages/Home"
+import Auth from "../Pages/Auth"
+import Category  from "../Pages/Category"
+import ProductDetails  from "../Pages/ProductDetails"
+
+export {Home, Auth, Category, ProductDetails} 
