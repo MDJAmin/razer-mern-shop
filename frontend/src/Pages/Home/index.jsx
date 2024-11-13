ff3dddc947f11d4e5b4ba0512dfa45c876154a27
MDJAmin <mohammadamindf@gmail.com>
2024-11-13 23:08:17 +0330

    Refactor : add content to homepage

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home-container">
+      <h1>Welcome to Home Page</h1>
+      <p>This is the home page content.</p>
+    </div>
+  );
 }
