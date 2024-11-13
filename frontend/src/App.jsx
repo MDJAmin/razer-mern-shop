2239ba73fdd74a3febb3fc64f0402d270fffde21
MDJAmin <mohammadamindf@gmail.com>
2024-11-13 23:08:40 +0330

    Refactor : Add lazy loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,67 +1,66 @@
 import { Route, Routes } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
 import PrivateRoute from "./Utils/PrivateRoute";
+import AdminPrivateRoute from "./Utils/AdminPrivateRoute";
 import ClientLayout from "./Layouts/ClientLayout";
 import AdminLayout from "./Layouts/AdminLayout";
-import {
-  Home,
-  Auth,
-  Category,
-  ProductDetails,
-  Products,
-  Card,
-  Pockets,
-  Profile,
-  Search,
-  ContactUs,
-  NotFound,
-} from "./Constants/client";
-import {
-  AddCategory,
-  AddProduct,
-  AddSlider,
-  AddDiscount,
-  AddProductVariant,
-  AddVariant,
-  AdminPanel,
-  Comments,
-  Users,
-} from "./Constants/admin";
-import AdminPrivateRoute from "./Utils/AdminPrivateRoute";
+
+const Home = lazy(() => import("./Constants/client/Home"));
+const Auth = lazy(() => import("./Constants/client/Auth"));
+const Category = lazy(() => import("./Constants/client/Category"));
+const ProductDetails = lazy(() => import("./Constants/client/ProductDetails"));
+const Products = lazy(() => import("./Constants/client/Products"));
+const Card = lazy(() => import("./Constants/client/Card"));
+const Pockets = lazy(() => import("./Constants/client/Pockets"));
+const Profile = lazy(() => import("./Constants/client/Profile"));
+const Search = lazy(() => import("./Constants/client/Search"));
+const ContactUs = lazy(() => import("./Constants/client/ContactUs"));
+const NotFound = lazy(() => import("./Constants/client/NotFound"));
+
+const AddCategory = lazy(() => import("./Constants/admin/AddCategory"));
+const AddProduct = lazy(() => import("./Constants/admin/AddProduct"));
+const AddSlider = lazy(() => import("./Constants/admin/AddSlider"));
+const AddDiscount = lazy(() => import("./Constants/admin/AddDiscount"));
+const AddProductVariant = lazy(() => import("./Constants/admin/AddProductVariant"));
+const AddVariant = lazy(() => import("./Constants/admin/AddVariant"));
+const AdminPanel = lazy(() => import("./Constants/admin/AdminPanel"));
+const Comments = lazy(() => import("./Constants/admin/Comments"));
+const Users = lazy(() => import("./Constants/admin/Users"));
 
 export default function App() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route element={<ClientLayout />}>
-          <Route exact path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/product" element={<Products />} />
-          <Route path="/product-details/:id" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/search" element={<Search />} />
-          <Route path="/category" element={<Category />} />
+          <Route path="/categories" element={<Category />} />
           <Route element={<PrivateRoute />}>
-            <Route path="/card" element={<Card />} />
-            <Route path="/pocket" element={<Pockets />} />
+            <Route path="/cart" element={<Card />} />
+            <Route path="/pockets" element={<Pockets />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/contact-us" element={<ContactUs />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Route>
         <Route element={<AdminPrivateRoute />}>
           <Route path="/admin" element={<AdminLayout />}>
             <Route index element={<AdminPanel />} />
-            <Route path="product" element={<AddProduct />} />
-            <Route path="product-variant" element={<AddProductVariant />} />
-            <Route path="variant" element={<AddVariant />} />
-            <Route path="category" element={<AddCategory />} />
-            <Route path="slider" element={<AddSlider />} />
-            <Route path="user" element={<Users />} />
-            <Route path="discount" element={<AddDiscount />} />
-            <Route path="comment" element={<Comments />} />
+            <Route path="products" element={<AddProduct />} />
+            <Route path="products/variants" element={<AddProductVariant />} />
+            <Route path="variants" element={<AddVariant />} />
+            <Route path="categories" element={<AddCategory />} />
+            <Route path="sliders" element={<AddSlider />} />
+            <Route path="users" element={<Users />} />
+            <Route path="discounts" element={<AddDiscount />} />
+            <Route path="comments" element={<Comments />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Route>
       </Routes>
-    </>
+    </Suspense>
   );
 }
