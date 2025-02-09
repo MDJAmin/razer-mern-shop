23d3db1d7f90ed75ba9e93fb409d2bac71315b19
MDJAmin <mohammadamindf@gmail.com>
2025-02-09 20:50:55 +0330

    Style: tailwind config

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import ThemeToggle from '../../Components/Common/Button/ThemeToggle'
 
 export default function Home() {
   return (
-    <div className='text-3xl underline'>Home</div>
+    <>
+    <ThemeToggle/> 
+    <div className='text-3xl underline bg-light dark:bg-dark text-light dark:text-white'>Home</div>
+    </>
   )
 }
