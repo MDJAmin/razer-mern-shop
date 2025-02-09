b1abe085492414cee525cffba9680a3634161261
MDJAmin <mohammadamindf@gmail.com>
2025-02-09 20:50:26 +0330

    Feat: Main
    add GlobalTheme for theme toggeling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -6,12 +6,28 @@ import { store, persistor } from "./Context/Store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import {BrowserRouter} from "react-router-dom"
+import {useSelector } from 'react-redux';
+
+const GlobalTheme = () => {
+  const theme = useSelector((state) => state.theme.theme);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  return null;
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
         <BrowserRouter>
+        <GlobalTheme />
           <App />
         </BrowserRouter>
       </Provider>
