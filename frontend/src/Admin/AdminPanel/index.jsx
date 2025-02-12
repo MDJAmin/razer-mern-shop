0f2b2148b006d2362e912b50f94b5dbbb035b597
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-02-12 08:44:23 +0330

    fix: improve admin panel

--- frontend/src/Admin/AdminPanel/index.jsx
+++ frontend/src/Admin/AdminPanel/index.jsx
@@ -6,6 +6,13 @@ export default function AdminPanel() {
   const { email } = useSelector((state) => state.user.currentUser);
   const shortMail = email.split("@")[0];
 
+  const containerClass =
+    "bg-white-smoke bg-opacity-50 dark:bg-admin-green dark:text-white text-xl py-24 text-center rounded-lg";
+  const gridItem1Class =
+    "col-span-12 md:col-span-12 lg:col-span-9 xl:col-span-5";
+  const gridItem2Class =
+    "col-span-12 md:col-span-6 lg:col-span-3 xl:col-span-2";
+
   return (
     <div className="flex flex-col items-start justify-start gap-2 w-full px-6 py-4">
       <div className="bg-white-smoke bg-opacity-50 dark:bg-admin-green py-4 px-6 flex flex-wrap gap-x-12 justify-between w-full text-xl rounded-lg">
@@ -15,23 +22,23 @@ export default function AdminPanel() {
         <CurrentDate />
       </div>
       <div className="grid grid-cols-12 gap-2 w-full">
-        <div className="col-span-12 md:col-span-12 lg:col-span-9 xl:col-span-5 bg-white-smoke bg-opacity-50 dark:bg-admin-green dark:text-white text-xl py-24 text-center rounded-lg">
+        <div className={`${gridItem1Class} ${containerClass}`}>
           Item 1 (2 Cols)
         </div>
-        <div className="col-span-12 md:col-span-6 lg:col-span-3 xl:col-span-2 bg-white-smoke bg-opacity-50 dark:bg-admin-green dark:text-white text-xl py-24 text-center rounded-lg">
+        <div className={`${gridItem2Class} ${containerClass}`}>
           Item 2 (1 Col)
         </div>
-        <div className="col-span-12 md:col-span-12 lg:col-span-9 xl:col-span-5 bg-white-smoke bg-opacity-50 dark:bg-admin-green dark:text-white text-xl py-24 text-center rounded-lg">
-          Item 3 (2 Col)
+        <div className={`${gridItem1Class} ${containerClass}`}>
+          Item 3 (2 Cols)
         </div>
-        <div className="col-span-12 md:col-span-12 lg:col-span-9 xl:col-span-5 bg-white-smoke bg-opacity-50 dark:bg-admin-green dark:text-white text-xl py-24 text-center rounded-lg">
-          Item 1 (2 Cols)
+        <div className={`${gridItem1Class} ${containerClass}`}>
+          Item 4 (2 Cols)
         </div>
-        <div className="col-span-12 md:col-span-6 lg:col-span-3 xl:col-span-2 bg-white-smoke bg-opacity-50 dark:bg-admin-green dark:text-white text-xl py-24 text-center rounded-lg">
-          Item 2 (1 Col)
+        <div className={`${gridItem2Class} ${containerClass}`}>
+          Item 5 (1 Col)
         </div>
-        <div className="col-span-12 md:col-span-12 lg:col-span-9 xl:col-span-5 bg-white-smoke bg-opacity-50 dark:bg-admin-green dark:text-white text-xl py-24 text-center rounded-lg">
-          Item 3 (2 Col)
+        <div className={`${gridItem1Class} ${containerClass}`}>
+          Item 6 (2 Cols)
         </div>
       </div>
     </div>
