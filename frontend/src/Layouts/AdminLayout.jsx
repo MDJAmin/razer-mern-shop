a0b6a7031097323bb405cbfddb12f6e2a470069e
MDJAmin <mohammadamindf@gmail.com>
2025-02-13 22:49:57 +0330

    OMG! ☕

--- frontend/src/Layouts/AdminLayout.jsx
+++ frontend/src/Layouts/AdminLayout.jsx
@@ -1,25 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { Outlet } from "react-router-dom";
 import IconBar from "../Components/Admin/IconBar";
 import MainNavBar from "../Constants/Components/MainNavbar";
 
+const MemoizedMainNavBar = React.memo(MainNavBar);
+const MemoizedIconBar = React.memo(IconBar);
+
 export default function AdminLayout() {
   const [showText, setShowText] = useState(false);
 
-  function handleShowText() {
-    setShowText(!showText);
-  }
+  const handleShowText = useCallback(() => {
+    setShowText((prev) => !prev);
+  }, []);
+
   return (
-    <>
-      <div className="bg-light-bg dark:bg-black-bg min-h-screen">
-        <div className="border-b-[1px] border-gray border-opacity-50 dark:border-admin-green">
-          <MainNavBar handleShowText={handleShowText} isAdmin={true} />
-        </div>
-        <div className="flex justify-start items-start xl:min-h-[calc(100vh-73px)]">
-          <IconBar showText={showText} />
-          <Outlet />
-        </div>
+    <div className="bg-light-bg dark:bg-black-bg min-h-screen">
+      <div className="border-b-[1px] border-gray border-opacity-50 dark:border-admin-green">
+        <MemoizedMainNavBar handleShowText={handleShowText} isAdmin={true} />
       </div>
-    </>
+      <div className="flex justify-start items-start xl:min-h-[calc(100vh-73px)]">
+        <MemoizedIconBar showText={showText} />
+        <Outlet /> 
+      </div>
+    </div>
   );
 }
