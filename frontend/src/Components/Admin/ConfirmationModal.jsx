41e0c9558d7c42b52f9f790c85c3c479a2455f02
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-02-20 13:26:23 +0330

    style: responsive confirm modal

--- frontend/src/Components/Admin/ConfirmationModal.jsx
+++ frontend/src/Components/Admin/ConfirmationModal.jsx
@@ -9,7 +9,7 @@ const ConfirmationModal = ({ isVisible, onClose, onConfirm, message }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-light dark:bg-dark dark:bg-opacity-60 bg-opacity-60 p-1">
       <div className="bg-light-bg dark:bg-black-bg p-4 rounded-md drop-shadow-lg">
-        <h2 className="text-2xl mb-6 dark:text-light">{message}</h2>
+        <h2 className="text-lg sm:text-2xl mb-6 dark:text-light">{message}</h2>
         <div className="flex gap-2">
           <button onClick={onClose} className="authBtn bg-error dark:bg-error">
             {t("cancel")}
