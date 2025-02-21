1b731ddf32361b43e0b8aaf00a70b953522ceefa
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-02-21 13:26:32 +0330

    Refactor: radarOptions
    setup color for radar chart

--- frontend/src/Admin/AdminPanel/index.jsx
+++ frontend/src/Admin/AdminPanel/index.jsx
@@ -34,7 +34,7 @@ ChartJS.register(
   RadialLinearScale
 );
 
-ChartJS.overrides.radar.color="#FFFFFF"
+ChartJS.overrides.radar.color = "#FFFFFF";
 
 export default function AdminPanel() {
   const { t } = useTranslation();
@@ -71,12 +71,40 @@ export default function AdminPanel() {
     },
   };
 
+  const radarOptions = {
+    plugins: {
+      legend: {
+        labels: {
+          color: theme === "dark" ? "#FFFFFF" : "#2C2C2C",
+        },
+      },
+      tooltip: {
+        titleColor: theme === "dark" ? "#FFFFFF" : "#2C2C2C",
+        bodyColor: theme === "dark" ? "#FFFFFF" : "#2C2C2C",
+      },
+    },
+    scales: {
+      r: {
+        grid: {
+          color: theme === "dark" ? "#acadac5b" : "#ACADAC",
+        },
+        angleLines: {
+          color: theme === "dark" ? "#acadac5b" : "#ACADAC",
+        },
+        pointLabels: {
+          color: theme === "dark" ? "#FFFFFF" : "#2C2C2C",
+        },
+        ticks: {
+          color: theme === "dark" ? "#000000" : "#000000",
+        },
+      },
+    },
+  };
+
   const containerClass =
     "bg-admin-gray dark:bg-admin-green text-dark dark:text-light rounded-lg";
-  const gridItem1Class =
-    "col-span-12 md:col-span-12 lg:col-span-9 xl:col-span-5";
-  const gridItem2Class =
-    "col-span-12 md:col-span-6 lg:col-span-3 xl:col-span-2";
+  const gridItem1Class = "col-span-12 md:col-span-12 lg:col-span-9 xl:col-span-5";
+  const gridItem2Class = "col-span-12 md:col-span-6 lg:col-span-3 xl:col-span-2";
 
   const barData = {
     labels: [
@@ -169,15 +197,7 @@ export default function AdminPanel() {
   };
 
   const radarData = {
-    labels: [
-      "Online",
-      "Store",
-      "Order",
-      "Shopping",
-      "Delivery",
-      "Purchase",
-      "Tips",
-    ],
+    labels: ["Online", "Store", "Order", "Shopping", "Delivery", "Purchase", "Tips"],
     datasets: [
       {
         label: "Eranings",
@@ -193,53 +213,53 @@ export default function AdminPanel() {
   };
 
   return (
-    <div className="flex flex-col items-start justify-start gap-2 w-full px-6 py-4 pb-12">
-      <div className="bg-admin-gray dark:bg-admin-green py-4 px-6 flex flex-wrap gap-x-12 justify-between w-full text-xl rounded-lg">
-        <h2 className="text-xl sm:text-2xl text-dark dark:text-light">
+    <div className='flex flex-col items-start justify-start gap-2 w-full px-6 py-4 pb-12'>
+      <div className='bg-admin-gray dark:bg-admin-green py-4 px-6 flex flex-wrap gap-x-12 justify-between w-full text-xl rounded-lg'>
+        <h2 className='text-xl sm:text-2xl text-dark dark:text-light'>
           {t("welcomeDear")} "{email ? shortMail : phone}"
         </h2>
         <CurrentDate />
       </div>
-      <div className="grid grid-cols-12 gap-2 w-full">
+      <div className='grid grid-cols-12 gap-2 w-full'>
         <div className={`${gridItem1Class} ${containerClass}`}>
-          <div className="flex flex-col justify-start items-start gap-8 p-6">
-            <div className="flex flex-col gap-1 items-start">
-              <h3 className="text-xl">Today's Sales</h3>
-              <p className="opacity-40 text-sm">Sales Summary</p>
+          <div className='flex flex-col justify-start items-start gap-8 p-6'>
+            <div className='flex flex-col gap-1 items-start'>
+              <h3 className='text-xl'>Today's Sales</h3>
+              <p className='opacity-40 text-sm'>Sales Summary</p>
             </div>
 
-            <div className="flex w-full flex-wrap gap-1 justify-between items-center">
-              <div className="flex flex-col gap-1 items-start text-dark dark:text-light bg-light-bg dark:bg-black-bg rounded-xl py-4 px-6">
-                <p className="text-amber-400 text-4xl mb-2">
+            <div className='flex w-full flex-wrap gap-1 justify-between items-center'>
+              <div className='flex flex-col gap-1 items-start text-dark dark:text-light bg-light-bg dark:bg-black-bg rounded-xl py-4 px-6'>
+                <p className='text-amber-400 text-4xl mb-2'>
                   <FaChartLine />
                 </p>
-                <p className="text-2xl">$5K</p>
-                <p className="text-[16px]">Total Sales</p>
-                <p className="text-amber-400 text-xs">+10% from yesterday</p>
+                <p className='text-2xl'>$5K</p>
+                <p className='text-[16px]'>Total Sales</p>
+                <p className='text-amber-400 text-xs'>+10% from yesterday</p>
               </div>
-              <div className="flex flex-col gap-1 items-start text-dark dark:text-light bg-light-bg dark:bg-black-bg rounded-xl py-4 px-6">
-                <p className="text-cyan-600 text-4xl mb-2">
+              <div className='flex flex-col gap-1 items-start text-dark dark:text-light bg-light-bg dark:bg-black-bg rounded-xl py-4 px-6'>
+                <p className='text-cyan-600 text-4xl mb-2'>
                   <LuClipboardCheck />
                 </p>
-                <p className="text-2xl">500</p>
-                <p className="text-[16px]">Total Order</p>
-                <p className="text-cyan-600 text-xs">+8% from yesterday</p>
+                <p className='text-2xl'>500</p>
+                <p className='text-[16px]'>Total Order</p>
+                <p className='text-cyan-600 text-xs'>+8% from yesterday</p>
               </div>
-              <div className="flex flex-col gap-1 items-start text-dark dark:text-light bg-light-bg dark:bg-black-bg rounded-xl py-4 px-6">
-                <p className="text-purple-300 text-4xl mb-2">
+              <div className='flex flex-col gap-1 items-start text-dark dark:text-light bg-light-bg dark:bg-black-bg rounded-xl py-4 px-6'>
+                <p className='text-purple-300 text-4xl mb-2'>
                   <BsBagCheck />
                 </p>
-                <p className="text-2xl">19</p>
-                <p className="text-[16px]">Product Sold</p>
-                <p className="text-purple-300 text-xs">+2% from yesterday</p>
+                <p className='text-2xl'>19</p>
+                <p className='text-[16px]'>Product Sold</p>
+                <p className='text-purple-300 text-xs'>+2% from yesterday</p>
               </div>
-              <div className="flex flex-col gap-1 items-start text-dark dark:text-light bg-light-bg dark:bg-black-bg rounded-xl py-4 px-6">
-                <p className="text-lime-400 text-4xl mb-2">
+              <div className='flex flex-col gap-1 items-start text-dark dark:text-light bg-light-bg dark:bg-black-bg rounded-xl py-4 px-6'>
+                <p className='text-lime-400 text-4xl mb-2'>
                   <FiUserPlus />
                 </p>
-                <p className="text-2xl">24</p>
-                <p className="text-[16px]">New Customer</p>
-                <p className="text-lime-400 text-xs">+3% from yesterday</p>
+                <p className='text-2xl'>24</p>
+                <p className='text-[16px]'>New Customer</p>
+                <p className='text-lime-400 text-xs'>+3% from yesterday</p>
               </div>
             </div>
           </div>
@@ -247,41 +267,34 @@ export default function AdminPanel() {
         <div
           className={`${gridItem2Class} ${containerClass} p-2 flex flex-col justify-evenly items-center`}
         >
-          <h2 className="text-start w-full text-xl px-2">Levels:</h2>
+          <h2 className='text-start w-full text-xl px-2'>Levels:</h2>
           <Radar
             data={radarData}
-            options={{
-              plugins: {
-                legend: {
-                  labels: {
-                    color: theme === "dark" ? "#FFFFFF" : "#000000",
-                  },
-                },
-              },
-              
-            }}
+            options={radarOptions}
           />
         </div>
         <div
           className={`${gridItem1Class} ${containerClass} p-4 flex flex-col justify-center items-center`}
         >
-          <h2 className="text-start w-full text-xl p-2 pb-0">Top Products:</h2>
-          <Bar data={barData} options={options} />
+          <h2 className='text-start w-full text-xl p-2 pb-0'>Top Products:</h2>
+          <Bar
+            data={barData}
+            options={options}
+          />
         </div>
         <div
           className={`${gridItem1Class} ${containerClass} p-4 flex flex-col justify-center items-center`}
         >
-          <h2 className="text-start w-full text-xl p-2 pb-0">
-            Customer Fulfilment:
-          </h2>
-          <Line data={lineDataSec} options={options} />
+          <h2 className='text-start w-full text-xl p-2 pb-0'>Customer Fulfilment:</h2>
+          <Line
+            data={lineDataSec}
+            options={options}
+          />
         </div>
         <div
           className={`${gridItem2Class} ${containerClass} p-2 flex flex-col justify-evenly items-center`}
         >
-          <h2 className="text-start w-full text-xl px-2">
-            Yearly Information:
-          </h2>
+          <h2 className='text-start w-full text-xl px-2'>Yearly Information:</h2>
           <Pie
             data={pieData}
             options={{
@@ -299,7 +312,10 @@ export default function AdminPanel() {
         <div
           className={`${gridItem1Class} ${containerClass} p-4 flex justify-center items-center`}
         >
-          <Line data={lineData} options={options} />
+          <Line
+            data={lineData}
+            options={options}
+          />
         </div>
       </div>
     </div>
